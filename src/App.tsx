10bfe939bc5d9eb2e23c06731ba1fc4220ec842a
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-06 15:10:37 -0700

    WIP. Initial layout. sections

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
     return (
-        <div className="">
-            11
-            <button className="bg-red-300">111</button>
+        <div className="h-screen bg-blue-50 flex-col text-blue-200">
+            <section className="mb-4" style={{boxShadow: '0px 2px 1px rgba(0,0,0, .2)'}}>
+                <header className="py-2 text-2xl bg-blue-500 flex">
+                    Redux
+                </header>
+            </section>
+            <section className="mb-4" style={{boxShadow: '0px 2px 1px rgba(0,0,0, .2)'}}>
+                <header className="py-2 text-2xl bg-blue-500 flex">
+                    Context Provider/Consumer
+                </header>
+            </section>
+            <section className="mb-4" style={{boxShadow: '0px 2px 1px rgba(0,0,0, .2)'}}>
+                <header className="py-2 text-2xl bg-blue-500 flex">
+                    Zustand
+                </header>
+            </section>
+            <section className="mb-4" style={{boxShadow: '0px 2px 1px rgba(0,0,0, .2)'}}>
+                <header className="py-2 text-2xl bg-blue-500 flex">
+                    Jotai / Recoil
+                </header>
+            </section>
         </div>
     );
 }
