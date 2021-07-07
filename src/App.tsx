780a834d43770572deab14a75cd21943a1ac83ba
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-06 17:36:24 -0700

    WIP. Initial layout. Logos: context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useState } from 'react';
 import './App.css';
-import { LogoReact, LogoRedux } from './components/XtraLogos';
+import { LogoReact, LogoContext, LogoRedux } from './components/XtraLogos';
 
 function Section({ logo, title, children }: { logo: ReactNode, title: string; children: ReactNode; }) {
     return (
@@ -19,10 +19,10 @@ function Section({ logo, title, children }: { logo: ReactNode, title: string; ch
 function App() {
     return (
         <div className="h-screen bg-blue-50 flex-col text-blue-200">
-            <Section title="Redux" logo={LogoReact()}>
+            <Section title="Redux" logo={LogoRedux()}>
 
             </Section>
-            <Section title="Context Provider/Consumer"  logo={LogoRedux()}>
+            <Section title="Context Provider/Consumer"  logo={LogoContext()}>
 
             </Section>
             <Section title="Zustand"  logo={LogoReact()}>
