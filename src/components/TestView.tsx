1de6a641193e5378decd61c15f3c2d6d14748b84
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-08 10:58:59 -0700

    Add circle for inner radius visualization as well

--- src/components/TestView.tsx
+++ src/components/TestView.tsx
@@ -10,6 +10,7 @@ function TestView(props: ShapeProps) {
         <svg className="w-full h-full" viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}>
             <path stroke="currentColor" strokeWidth="2" fill="none" d={path} />
             <circle stroke="#00ff0080" strokeWidth="1" fill="none" r={oRadius} cx={viewBoxSize / 2} cy={viewBoxSize / 2} />
+            <circle stroke="#00ff0080" strokeWidth="1" fill="none" r={iRadius} cx={viewBoxSize / 2} cy={viewBoxSize / 2} />
         </svg>
     );
 }
