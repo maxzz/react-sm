cdc6c61e9db5bdb6b8996fc70484bfd1c5d5dc87
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-07 17:20:31 -0700

    Fix the inital shape angle

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -14,10 +14,14 @@ function generatePoints(numberOfPoints: number, outerRadius: number, innerRadius
     const angleStep = (Math.PI * 2) / numberOfPoints;
     const points = [];
 
-    for (let i = 1; i <= numberOfPoints; i++) {
+    const initialAngle = -Math.PI / 2; //-90 * Math.PI / 180
+
+    for (let i = 0; i < numberOfPoints; i++) {
+        console.log('a', i, i * angleStep);
+
         const radiusAtPoint = i % 2 === 0 ? outerRadius : innerRadius;
-        const x = xPosition + Math.cos(i * angleStep) * radiusAtPoint;
-        const y = yPosition + Math.sin(i * angleStep) * radiusAtPoint;
+        const x = xPosition + Math.cos(i * angleStep + initialAngle) * radiusAtPoint;
+        const y = yPosition + Math.sin(i * angleStep + initialAngle) * radiusAtPoint;
         points.push({ x, y });
     }
 
