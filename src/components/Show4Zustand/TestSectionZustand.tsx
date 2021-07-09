e3c42213618b2ccdab95ee2a8f1dc4c369384e72
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-09 00:06:41 -0700

    WIP. refactoring. TestControls as a separate component

--- src/components/Show4Zustand/TestSectionZustand.tsx
+++ src/components/Show4Zustand/TestSectionZustand.tsx
@@ -3,6 +3,7 @@ import { ShapeProps } from '../../utils/numbers';
 import TestView, { viewBoxSize } from '../TestView';
 import create from 'zustand';
 import shallow from 'zustand/shallow';
+import TestControls from '../TestControls';
 
 function DeepTreeSimulation(props: ShapeProps) {
     return (
@@ -19,35 +20,13 @@ function TestSectionView() {
         setORadius: store.setORadius,
         setIRadius: store.setIRadius
     }), shallow);
-    // const [nPoints, setNPoints] = useAtom(nPointsAtom);
-    // const [oRadius, setORadius] = useAtom(oRadiusAtom);
-    // const [iRadius, setIRadius] = useAtom(iRadiusAtom);
+
+    let props = { nPoints, setNPoints, oRadius, setORadius, iRadius, setIRadius, };
 
     return (
-        <div className="px-4 py-2 flex space-x-4">
-            {/* View */}
-            <div className="w-32 h-32 text-blue-500 border-4 rounded-md border-blue-100 bg-gray-50">
-                <DeepTreeSimulation oRadius={oRadius} iRadius={iRadius} nPoints={nPoints} />
-            </div>
-            {/* Controls */}
-            <div className="text-blue-900">
-                <div className="flex items-center">
-                    <div className="flex-1">Outer points</div>
-                    <input className="mx-4" type="range" min={0} max={50} value={nPoints} onChange={(event) => setNPoints(+event.target.value)} />
-                    <div className="w-16">{nPoints}</div>
-                </div>
-                <div className="flex items-center">
-                    <div className="flex-1">Inner radius</div>
-                    <input className="mx-4" type="range" min={1} max={viewBoxSize / 2} value={iRadius} onChange={(event) => setIRadius(+event.target.value)} />
-                    <div className="w-16">{iRadius}</div>
-                </div>
-                <div className="flex items-center">
-                    <div className="flex-1">Outer radius</div>
-                    <input className="mx-4" type="range" min={1} max={viewBoxSize / 2} value={oRadius} onChange={(event) => setORadius(+event.target.value)} />
-                    <div className="w-16">{oRadius}</div>
-                </div>
-            </div>
-        </div>
+        <TestControls {...props} body={
+            <DeepTreeSimulation oRadius={oRadius} iRadius={iRadius} nPoints={nPoints} />
+        } />
     );
 }
 
