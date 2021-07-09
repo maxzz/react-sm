953681c849a3168c4f4f26975d903bdfb2f1f468
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-08 23:51:47 -0700

    WIP. refactoring. TestControls as a separate component

---
+++ src/components/TestControls.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { ShapeProps } from '../utils/numbers';
+import TestView, { viewBoxSize } from './TestView';
+
+export type TestControlsProps = {
+    nPoints: number;
+    oRadius: number;
+    iRadius: number;
+    setNPoints: (v: number) => void;
+    setORadius: (v: number) => void;
+    setIRadius: (v: number) => void;
+};
+
+function DeepTreeSimulation0(props: ShapeProps) {
+    return <TestView {...props} />;
+}
+
+function DeepTreeSimulation(props: ShapeProps) {
+    return (
+        <DeepTreeSimulation0 {...props} />
+    );
+}
+
+function TestControls(props: TestControlsProps) {
+    const { nPoints, setNPoints, oRadius, setORadius, iRadius, setIRadius, } = props;
+
+    return (<div className="px-4 py-2 flex space-x-4">
+        {/* View */}
+        <div className="w-32 h-32 text-blue-500 border-4 rounded-md border-blue-100 bg-gray-50">
+            <DeepTreeSimulation oRadius={oRadius} iRadius={iRadius} nPoints={nPoints} />
+        </div>
+        {/* Controls */}
+        <div className="text-blue-900">
+            <div className="flex items-center">
+                <div className="flex-1">Outer points</div>
+                <input className="mx-4" type="range" min={0} max={50} value={nPoints} onChange={(event) => setNPoints(+event.target.value)} />
+                <div className="w-16">{nPoints}</div>
+            </div>
+            <div className="flex items-center">
+                <div className="flex-1">Inner radius</div>
+                <input className="mx-4" type="range" min={1} max={viewBoxSize / 2} value={iRadius} onChange={(event) => setIRadius(+event.target.value)} />
+                <div className="w-16">{iRadius}</div>
+            </div>
+            <div className="flex items-center">
+                <div className="flex-1">Outer radius</div>
+                <input className="mx-4" type="range" min={1} max={viewBoxSize / 2} value={oRadius} onChange={(event) => setORadius(+event.target.value)} />
+                <div className="w-16">{oRadius}</div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+
+export default TestControls;
