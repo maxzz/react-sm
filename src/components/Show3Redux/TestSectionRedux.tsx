356ad9174a14da81499d7a46bdca4ec41edb82ce
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-09 00:04:33 -0700

    WIP. refactoring. TestControls as a separate component

--- src/components/Show3Redux/TestSectionRedux.tsx
+++ src/components/Show3Redux/TestSectionRedux.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ShapeProps } from '../../utils/numbers';
-import TestView, { viewBoxSize } from '../TestView';
+import TestView from '../TestView';
 
 import { Provider } from 'react-redux';
 import { store } from './store';
@@ -16,7 +16,6 @@ function DeepTreeSimulation(props: ShapeProps) {
 }
 
 function TestContextView() {
-
     const nPoints = useAppSelector((store) => store.shapeProps.nPoints);
     const oRadius = useAppSelector((store) => store.shapeProps.oRadius);
     const iRadius = useAppSelector((store) => store.shapeProps.iRadius);
@@ -33,30 +32,6 @@ function TestContextView() {
         <TestControls {...props} body={
             <DeepTreeSimulation oRadius={oRadius} iRadius={iRadius} nPoints={nPoints} />
         } />
-        // <div className="px-4 py-2 flex space-x-4">
-        //     {/* View */}
-        //     <div className="w-32 h-32 text-blue-500 border-4 rounded-md border-blue-100 bg-gray-50">
-        //         <DeepTreeSimulation oRadius={oRadius} iRadius={iRadius} nPoints={nPoints} />
-        //     </div>
-        //     {/* Controls */}
-        //     <div className="text-blue-900">
-        //         <div className="flex items-center">
-        //             <div className="flex-1">Outer points</div>
-        //             <input className="mx-4" type="range" min={0} max={50} value={nPoints} onChange={(event) => setNPoints(+event.target.value)} />
-        //             <div className="w-16">{nPoints}</div>
-        //         </div>
-        //         <div className="flex items-center">
-        //             <div className="flex-1">Inner radius</div>
-        //             <input className="mx-4" type="range" min={1} max={viewBoxSize / 2} value={iRadius} onChange={(event) => setIRadius(+event.target.value)} />
-        //             <div className="w-16">{iRadius}</div>
-        //         </div>
-        //         <div className="flex items-center">
-        //             <div className="flex-1">Outer radius</div>
-        //             <input className="mx-4" type="range" min={1} max={viewBoxSize / 2} value={oRadius} onChange={(event) => setORadius(+event.target.value)} />
-        //             <div className="w-16">{oRadius}</div>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
